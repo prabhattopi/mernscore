bb5bfde1d6d7795ecc9a9c6be4ea004d39b19759
prabhattopi <topi9864@gmail.com>
2022-08-27 01:25:54 +0530

    The file

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+
+const path=require("path")
+const express =require("express")
+const colors=require("colors")
+const dotenv=require("dotenv").config()
+const port=process.env.PORT || 4000
+const {errorHandle}=require("./middleware/errorMiddleware")
+const connectDB=require("./config/db")
+
+connectDB()
+const app=express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:false}))
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+app.use("/api/goals",require("./routes/goalRoutes"))
+app.use("/api/users",require("./routes/userRoutes"))
+
+
+app.use(errorHandle)
+
+app.listen(port,()=>console.log(`Server stated on port ${port}`))
+
+
